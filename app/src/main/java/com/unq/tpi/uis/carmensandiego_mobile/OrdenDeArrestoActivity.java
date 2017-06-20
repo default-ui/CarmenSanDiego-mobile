a6ddb1126b8565a059baaa5df6a55d29a3351adc
tini <agustinariq@gmail.com>
2017-06-20 18:46:17 -0300

    Agregada activity orden de arresto

---
+++ app/src/main/java/com/unq/tpi/uis/carmensandiego_mobile/OrdenDeArrestoActivity.java
@@ -0,0 +1,15 @@
+package com.unq.tpi.uis.carmensandiego_mobile;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrdenDeArrestoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orden_de_arresto);
+
+    }
+}
--- app/src/main/java/com/unq/tpi/uis/carmensandiego_mobile/ResolverMisterioActivity.java
+++ app/src/main/java/com/unq/tpi/uis/carmensandiego_mobile/ResolverMisterioActivity.java
@@ -1,8 +1,10 @@
 package com.unq.tpi.uis.carmensandiego_mobile;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ResolverMisterioActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class ResolverMisterioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resolver_misterio);
 
+    }
+
+    public void ordenDeArresto(View view){
+        Intent detailIntent = new Intent(this, OrdenDeArrestoActivity.class);
+        startActivity(detailIntent);
     }
 
 }
